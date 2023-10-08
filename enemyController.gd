37515899e2362ce0119df8c666510860cca58656
osid54 <osid.potato@gmail.com>
2023-10-07 20:51:45 -0400

    posarray for path

--- enemyController.gd
+++ enemyController.gd
@@ -1,6 +1,15 @@
 extends Node
 
 var enemyPath = []
+var tilePos = [Vector2(243, 778), 
+				Vector2(576, 879), 
+				Vector2(904, 724), 
+				Vector2(1284, 766), 
+				Vector2(1650, 864)]
+				
+var growth := [preload("res://src/sprites/plants/plantS.png"),
+	preload("res://src/sprites/plants/plantM.png"),
+	preload("res://src/sprites/plants/plantL.png")]
 
 func _ready():
 	Autoload.timeTick.connect(moveEnemies)
@@ -16,3 +25,11 @@ func moveEnemies():
 		else:
 			tempPath.append(enemyPath[i])
 	enemyPath = tempPath
+	for i in 5:
+		if enemyPath[i] != null:
+			enemyPath[i].walkTo(tilePos[i])
+			
+func spawnEnemies(path: Array):
+	for i in 5:
+		if enemyPath[i] != null:
+			enemyPath[i].walkTo(tilePos[i])
