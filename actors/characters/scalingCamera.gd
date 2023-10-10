e5c337e86297ae5f4c529d32c692303a92ab401b
osid54 <osid.potato@gmail.com>
2023-10-10 19:21:30 -0400

    scaling camera added

---
+++ actors/characters/scalingCamera.gd
@@ -0,0 +1,8 @@
+extends Camera2D
+
+@export var neededSize: = Vector2()
+
+func _ready():
+	var factor := Vector2(get_viewport().get_window().size.x / neededSize.x,
+	get_viewport().get_window().size.y / neededSize.y)
+	zoom = factor
