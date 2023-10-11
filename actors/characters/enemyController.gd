a8f32f6bec8cafe82555ec68d9d292092a2f0219
osid54 <osid.potato@gmail.com>
2023-10-10 20:31:03 -0400

    speech tiling

--- actors/characters/enemyController.gd
+++ actors/characters/enemyController.gd
@@ -24,7 +24,7 @@ func moveEnemies():
 				if enemyPath[i] != null:
 					Autoload.dead()
 					await enemyPath[i].walkTo(Vector2(-1228,938))
-					queue_free()
+					enemyPath[i].queue_free()
 			else:
 				tempPath.append(enemyPath[i])
 		enemyPath = tempPath
