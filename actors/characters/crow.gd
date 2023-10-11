103d67f3d3e8c384221d034c70fa5109548da50a
osid54 <osid.potato@gmail.com>
2023-10-11 13:15:54 -0400

    crowPointer

--- actors/characters/crow.gd
+++ actors/characters/crow.gd
@@ -1,4 +1,19 @@
 extends AnimatedSprite2D
 
 func _ready():
+	Autoload.loadLevel.connect(reset)
 	play("default")
+
+func reset():
+	$pointer.visible = false
+
+func interact():
+	$hint.visible = true
+	$pointer.visible = false
+
+func _on_area_2d_area_entered(_area):
+	if $hint.visible == false:
+		$pointer.visible = true
+
+func _on_area_2d_area_exited(_area):
+	$pointer.visible = false
--- actors/effects/pointer.gd
+++ actors/effects/pointer.gd
@@ -7,7 +7,7 @@ var basic = Color(1,1,1,1)
 var plant = Color8(25,54,40)
 var bag = Color8(53,99,42)
 var choice = Color8(162,191,94)
-var crow = Color8(31,33,55)
+var crow = Color8(37,65,100)
 var throw = Color8(129,39,55)
 var pump = Color8(201,119,67)
 
